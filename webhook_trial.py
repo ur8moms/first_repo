c479c1064e23b64b3e90ee16380f621e57028057
ur8moms <103761590+ur8moms@users.noreply.github.com>
2022-04-16 00:37:50 +0530

    Update webhook_trial.py

--- webhook_trial.py
+++ webhook_trial.py
@@ -1,9 +1,11 @@
-import praw
+#import praw
 from flask import Flask
 from flask import request
 from flask import json  #importing json cause that’s what we’re going to be working with
 import git,subprocess
 import traceback
+import os
+import signal
 
 #if self.git_is_installed():
 #            g = Git(options['path'])
@@ -20,13 +22,20 @@ def hook_root():
   global p_arr
   #if request.headers['Content-Types'] == 'application/json':  # calling json objects
   #print(request.json)
-  try: p_arr[-1].kill()
+  try:
+    g = open('pid.pkl', 'rb')
+    pid = pickle.load(g)
+    g.close()
+    os.kill(pid, signal.SIGTERM)
   except: 
     traceback.print_exc()
   g = git.cmd.Git('https://github.com/ur8moms/first_repo')
   g.pull('https://github.com/ur8moms/first_repo')
   p = subprocess.Popen(['python', 'first.py'])
-  p_arr.append(p)
+  pid=p.pid
+  f = open('../pid.pkl', 'wb')
+  pickle.dump(pid, f)
+  f.close()
   return json.dumps(request.json)
 
 if __name__ == '__main__':
