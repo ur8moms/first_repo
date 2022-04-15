c5f3722cf70a609683da15ec31120f268dcc2819
ur8moms <103761590+ur8moms@users.noreply.github.com>
2022-04-15 23:13:50 +0530

    Add files via upload

---
+++ webhook_trial.py
@@ -0,0 +1,26 @@
+import praw
+from flask import Flask
+from flask import request
+from flask import json  #importing json cause that’s what we’re going to be working with
+import git
+
+#if self.git_is_installed():
+#            g = Git(options['path'])
+#            g.init()
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+  return 'Hello World!'
+
+@app. route('/hooktest', methods=['POST'])  # ‘/hooktest’ specifies which link will it work on
+def hook_root():
+  #if request.headers['Content-Types'] == 'application/json':  # calling json objects
+  print(request.json)
+  g = git.cmd.Git('https://github.com/ur8moms/first_repo')
+  g.pull('https://github.com/ur8moms/first_repo')
+  return json.dumps(request.json)
+
+if __name__ == '__main__':
+  app.run(debug=True)
